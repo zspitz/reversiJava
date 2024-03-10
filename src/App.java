eb078a44050327831b1577dc99da61a4599ce922
Zev Spitz <shivisi@hotmail.com>
2024-03-10 13:25:35 +0200

    Add colors to draw

--- src/App.java
+++ src/App.java
@@ -7,9 +7,17 @@ public class App {
     }
 
     private static void draw(char[][] state) {
+        final String ANSI_BLACK = "\u001B[40m";
+        final String ANSI_WHITE = "\u001B[47m";
+        final String ANSI_GREEN = "\u001B[42m";
+        final String ANSI_RESET = "\u001B[0m";
+
         for (char[] row : state) {
             for (char cell : row) {
-                System.out.print(cell == '\0' ? ' ' : cell);
+                cell = cell == '\0' ? ' ' : cell;
+                final String toPrint = (cell == 'b' ? ANSI_BLACK : cell == 'w' ? ANSI_WHITE : ANSI_GREEN) + cell
+                        + ANSI_RESET;
+                System.out.print(toPrint);
             }
             System.out.println();
         }
